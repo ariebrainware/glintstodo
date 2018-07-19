eedd6eeac4cf96265478b6ae77bf62831ee9be3b
Arie Brainware <ariebrainware@gmail.com>
2018-07-19 16:41:27 +0700

    1.Add some comment code for learning purpose and remove todo.js for a while 2.Move button sign up outside form 3.Add signup.html 4.Add footer for signup.html , add some style for task list 5.Change algorthm for todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,12 @@
 //Author : Arie Brainware
 
 
-function addTask(){
-    var task = prompt("What do you want to do ?");
-    document.getElementById("task").innerHTML+="<input type='checkbox' placeholder='What do you want to do?'>"+task+"<br>";
+function addTask() {
+    var task = document.getElementById("task");
+    document.getElementById("list").innerHTML += "<button type='button' class='list-group-item list-group-item-action active text-center'>" +  task + "</button>";
 }
 
-function resetTask(){
-    document.getElementById("task").innerHTML="";
-    
+function resetTask() {
+    document.getElementById("list").innerHTML = "";
+
 }
